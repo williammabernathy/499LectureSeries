1a0b2c3ca51032cfa99996fa24ca02d7be67523b
Igifoshifo <matthew.abernathy@hotmail.com>
2019-11-05 22:44:20 -0500

    removed changes as of 11/5

--- src/williamabernathycsc499homework1/CompareOutput.java
+++ src/williamabernathycsc499homework1/CompareOutput.java
@@ -4,12 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Arrays;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
 
 public class CompareOutput 
 {
--- src/williamabernathycsc499homework1/WilliamAbernathyCSC499Homework1.java
+++ src/williamabernathycsc499homework1/WilliamAbernathyCSC499Homework1.java
@@ -10,16 +10,8 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.Scanner;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
 public class WilliamAbernathyCSC499Homework1 
 {
     private static Scanner input = new Scanner(System.in);
